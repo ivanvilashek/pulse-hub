e2a7c90cad0f0b92405a57ec02e8b97a8115f4b8
ivanvilashek <ivan.vilashek@gmail.com>
2024-03-16 18:00:59 +0200

    chore: update button component

--- app/ui/components/button/button.type.ts
+++ app/ui/components/button/button.type.ts
@@ -2,7 +2,12 @@ import { ButtonVariants } from './button.variants'
 
 export type ButtonVariant = keyof typeof ButtonVariants
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+export type ButtonProps = Omit<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  'disabled' | 'aria-disabled'
+> & {
   children: React.ReactNode
   variant?: ButtonVariant
+  isLoading?: boolean
+  isDisabled?: boolean
 }
