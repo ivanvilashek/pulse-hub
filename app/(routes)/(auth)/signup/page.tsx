5fd88b2ded717a9328ed1d1cc4b686092e3af1a8
ivanvilashek <ivan.vilashek@gmail.com>
2024-03-17 00:09:44 +0200

    chore: add auth pages

--- app/(routes)/(auth)/signup/page.tsx
+++ app/(routes)/(auth)/signup/page.tsx
@@ -1,25 +1,25 @@
 import { Routes } from '@app/lib/constants'
-import { LoginForm } from '@app/ui/auth/login-form'
+import { SignUpForm } from '@app/ui/auth/sign-up-form'
 import { Metadata } from 'next'
 import Link from 'next/link'
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'Login',
+  title: 'Register',
 }
 
 const Page = () => {
   return (
     <>
       <p className="text-center text-2xl font-bold text-gray-9">
-        Sign in to your account
+        Create new account
       </p>
       <div className="flex flex-col space-y-4">
-        <LoginForm />
+        <SignUpForm />
         <p className="text-center text-sm font-semibold text-gray-9">
-          <span>Not a member?</span>
-          <Link href={Routes.SIGN_UP} className="ml-1 text-primary">
-            Create new account
+          <span>Already have an account?</span>
+          <Link href={Routes.SIGN_IN} className="ml-1 text-primary">
+            Login
           </Link>
         </p>
       </div>
