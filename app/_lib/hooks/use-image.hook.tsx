af988a52b56a163f8469bc116c1943dcd5b0636e
ivanvilashek <ivan.vilashek@gmail.com>
2024-03-20 21:05:42 +0200

    chore: add use image hook

---
+++ app/_lib/hooks/use-image.hook.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useCallback, useEffect } from 'react'
+
+type NativeImageProps = React.ImgHTMLAttributes<HTMLImageElement>
+
+export type UseImageProps = {
+  src?: string
+  onLoad?: NativeImageProps['onLoad']
+  onError?: NativeImageProps['onError']
+}
+
+type ImageEvent = React.SyntheticEvent<HTMLImageElement, Event>
+
+export const useImage = ({ src, onLoad, onError }: UseImageProps) => {
+  const load = useCallback(() => {
+    if (!src) return
+
+    const img = new Image()
+    img.src = src
+
+    img.onload = (event) => {
+      onLoad?.(event as unknown as ImageEvent)
+    }
+
+    img.onerror = (error) => {
+      onError?.(error as unknown as ImageEvent)
+    }
+  }, [src, onLoad, onError])
+
+  useEffect(() => {
+    if (src) {
+      load()
+    }
+  }, [load, src])
+}
